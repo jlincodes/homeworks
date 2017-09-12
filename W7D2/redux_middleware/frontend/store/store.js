ab44fd3c128319d556e4f0b55c50a07def6fd6f7
Julie <julie.lin0812@gmail.com>
2017-09-12 07:48:02 -0700

    Initial commit of W7D2 homework

---
+++ W7D2/redux_middleware/frontend/store/store.js
@@ -0,0 +1,24 @@
+import { applyMiddleware, createStore } from 'redux';
+import RootReducer from '../reducers/root_reducer';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(
+    RootReducer,
+    preloadedState,
+    applyMiddleware(addLoggingToDispatch)
+  );
+
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+};
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+export default configureStore;
